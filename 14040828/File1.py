1402ee26d5da10043bf6600da5f9f4f89062e322
mmtorkaman0 <mmtorkaman0@gmail.com>
2025-11-19 12:36:51 +0000

    14040828

---
+++ 14040828/File1.py
@@ -0,0 +1,85 @@
+
+print("سلام دنیا!")
+
+
+name= "محمد"
+age = 34
+print("اسم من" + name + "و سنم" + str(age) + "هست")
+print(f"my name is {name} and my age is {age}")
+
+
+
+age = 50
+if age > 40:
+    print("To dige bozorgi")
+else:
+    print("hanooz koochiki")
+
+
+
+score = 30
+if score < 40:
+    print("ozat kharabe")
+else:
+    print("ozat khube")
+
+
+
+
+
+for i in range(3):
+    print(f"in shomare {i} hast")
+
+
+fruits=["Mive1","Mive2","Mive3"]
+for fruit in fruits:
+    print(f"miveye Morede Alaghe Man {fruit} Hast")
+
+
+
+fruits=["Mive1","Mive2","Mive3"]
+for fruit in fruits:
+    print(f"miveye Morede Alaghe Man {fruit} Hast", end=" ")
+
+
+
+
+
+print("-----shomaresh")
+for i in range(7):
+    print(i)
+
+
+print("-----shomaresh")
+for i in range(2,7):
+    print(i)
+    
+
+
+print("-----shomaresh")
+for i in range(2,7):
+    print(i, end=" ")
+
+
+
+
+print("!!!!!!!!!!shomaresh")
+for i in range(6):
+    print(i)
+    
+
+
+
+print("+++++shomaresh")
+for i in range(3,9):
+    print(i)
+
+
+
+print("//////shomaresh")
+for i in range(3,8):
+    print(i,end=" ")
+
+
+
+
